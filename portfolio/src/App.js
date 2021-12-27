d1508abe6a5a38059dba857c00e9c5030e1d4673
PeterJochem <peterjochem2020@u.northwestern.edu>
2021-12-27 11:03:10 -0500

    Cloned the Material-UI blog post example and have it working locally

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+//import './App.css';
+import Blog from './blog/Blog.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       	<Blog />
+      </header>
+    </div>
+  );
+}
+
+export default App;
