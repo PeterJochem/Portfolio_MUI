2f8772df26c55587df3696d4a656b65ed2d5f24d
PeterJochem <peterjochem2020@u.northwestern.edu>
2021-12-27 12:31:10 -0500

    Added a few posts

--- portfolio/src/blog/Blog.js
+++ portfolio/src/blog/Blog.js
@@ -19,29 +19,37 @@ import post3 from './blog-post.3.md';
 const sections = [];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Go Bot',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+    "5 DOF robot plays Go in Gazebo and real life",
+  image: 'https://github.com/PeterJochem/Go_Bot/blob/master/media/pick_place_demo.gif?raw=true',
   imageText: 'main image description',
   linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
   {
-    title: 'Featured post',
-    date: 'Nov 12',
+    title: 'Navigation Stack From Scratch',
+    date: 'Summer 2020',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      'Built the navigation software for a Turtlebot3',
+    image: 'https://github.com/PeterJochem/Turtlebot_Navigation/raw/master/images/pentagon.gif',
     imageLabel: 'Image Text',
   },
   {
-    title: 'Post title',
-    date: 'Nov 11',
+    title: 'Navigation Stack From Scratch',
+    date: 'Summer 2020',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      'Built the navigation software for a Turtlebot3',
+    image: 'https://github.com/PeterJochem/Turtlebot_Navigation/raw/master/images/pentagon.gif',
+    imageLabel: 'Image Text',
+  },
+  {
+    title: 'Building a Deep RL Library',
+    date: 'Fall 2020',
+    description:
+      'Implemented a smorgrasbord of deep reinorcement learning algorithms and OpenAI gym enviroments',
+    image: 'https://raw.githubusercontent.com/PeterJochem/Deep_RL/master/DDPG/media/hopper_learned_policy_cropped.gif',
     imageLabel: 'Image Text',
   },
 ];
@@ -52,19 +60,7 @@ const sidebar = {
   title: 'About',
   description:
     'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
-  ],
+  archives: [],
   social: [
     { name: 'GitHub', icon: GitHubIcon },
   ],
@@ -85,21 +81,11 @@ export default function Blog() {
               <FeaturedPost key={post.title} post={post} />
             ))}
           </Grid>
-          <Grid container spacing={5} sx={{ mt: 3 }}>
-            <Main title="From the firehose" posts={posts} />
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
-          </Grid>
+      
         </main>
-      </Container>
-      <Footer
-        title="Footer"
-        description="Something here to give the footer a purpose!"
-      />
+      </Container>	  
+	  
+	  {/* <Footer description="" /> */}	  
     </ThemeProvider>
   );
 }
