13e4caea9f3206501da9184e194241ff0806d5c7
PeterJochem <peterjochem2020@u.northwestern.edu>
2022-01-04 08:19:46 -0500

    Added images for each projects summary

--- portfolio/src/blog/Posts.js
+++ portfolio/src/blog/Posts.js
@@ -3,24 +3,33 @@
 let Go_Bot = {dynamicImage: "url(/images/go_bot.gif)", title: "Go Bot", subtitle: "Reinforcement Learning, ROS, MoveIt, FastAPI, React", 
 	description: "Developed reinforcement learning go playing robot. Robot can play over a network in either Gazebo or the real world"};
 
-let Turtle_Bot = {dynamicImage: "url(/images/go_bot.gif)", title: "ROS Navigation Stack", subtitle: "ROS, Navigation, C++", 
+let Turtle_Bot = {dynamicImage: "url(/images/turtle_bot_pentagon.gif)", title: "ROS Navigation Stack", subtitle: "ROS, Navigation, C++", 
         description: "Developed navigation software from scratch for mobile robot in ROS. Deployed onto Turtlebot"}
 
-let Deep_RL = {dynamicImage: "url(/images/go_bot.gif)", title: "Deep RL", subtitle: "Reinforcement Learning, Tensorflow",
+let Deep_RL = {dynamicImage: "url(/images/hopper_learned_policy.gif)", title: "Deep RL", subtitle: "Reinforcement Learning, Tensorflow",
         description: "Built a library of deep reinforcement learning algorithms and deployed them in OpenAI gyms."}
 
-let GAN = {dynamicImage: "url(/images/go_bot.gif)", title: "GAN", subtitle: "GANs, Tensorflow",
+let GAN = {dynamicImage: "url(/images/GAN_downsampled.gif)", title: "GAN", subtitle: "GANs, Tensorflow",
         description: "Built a generative adversarial network with Tensorflow"}
 
-let Pendulum = {dynamicImage: "url(/images/go_bot.gif)", title: "Triple Pendulum", subtitle: "Lagrangian Dynamics",
+let Pendulum = {dynamicImage: "url(/images/pend.gif)", title: "Triple Pendulum", subtitle: "Lagrangian Dynamics",
         description: "Built a triple pendulum simulation"}
 
-let Chess_AI = {dynamicImage: "url(/images/go_bot.gif)", title: "Chess Engine", subtitle: "Classic AI, Search Algorithms",
+let Chess_AI = {dynamicImage: "url(/images/chess.gif)", title: "Chess Engine", subtitle: "Classic AI, Search Algorithms",
         description: "Built a chess engine with an AI to play against"}
 
-let Mobile_Manipulator = {dynamicImage: "url(/images/go_bot.gif)", title: "Mobile Manipulation", subtitle: "VREP, Modern Robotics",
+let Mobile_Manipulator = {dynamicImage: "url(/images/kuka.gif)", title: "Mobile Manipulation", subtitle: "VREP, Modern Robotics",
+        description: ""}
+
+let RRT = {dynamicImage: "url(/images/rrt.png)", title: "Rapidly Exploring Random Tree", subtitle: "Planning Algorithms, Robotics",
+        description: ""}
+
+let Canny_Edge_Detector = {dynamicImage: "url(/images/Lena_Processed.png)", title: "Canny Edge Detector From Scratch", subtitle: "Computer Vision, Image Processing",
+        description: ""}
+
+let CBirch97 = {dynamicImage: "url(/images/CBirch97.gif)", title: "CBirch97 Head Tracking", subtitle: "Computer Vision, Image Processing",
         description: ""}
 
 
-let Posts = [Go_Bot, Turtle_Bot, Deep_RL, GAN, Pendulum, Chess_AI, Mobile_Manipulator]
+let Posts = [Go_Bot, Turtle_Bot, Deep_RL, GAN, Pendulum, Chess_AI, Mobile_Manipulator, RRT, Canny_Edge_Detector, CBirch97]
 export default Posts;
