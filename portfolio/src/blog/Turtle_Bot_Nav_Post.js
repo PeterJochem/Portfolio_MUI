c1b8a03ef1f114f3d55c2febb37d9e228c6c2b94
PeterJochem <peterjochem2020@u.northwestern.edu>
2022-01-16 08:54:56 -0500

    Added Router for descrption pages and external links where needed

---
+++ portfolio/src/blog/Turtle_Bot_Nav_Post.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import * as my_css from './a.css';
+import * as my_css2 from './b.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Header from './Header';
+import Box from '@material-ui/core/Box';
+import Posts from './Posts.js'; 
+
+const sections = [];
+
+export default function TBot_Nav_Post() {
+  return (
+	<React.Fragment>
+        <section className="hero hero--single">
+
+        <div className="hero__image" style={{backgroundImage: "url(/images/turtle_bot_pentagon.gif)"}}>
+                <div className="hero__overlay"></div>
+        </div>
+
+        <div className="wrap">
+
+                <h1>Go Bot</h1>
+                <p>C++, ROS, Motion Planning, MoveIt</p>
+        </div>
+
+	</section>
+
+	<section class="single">
+
+        <div class="wrap">
+
+                <article class="single-post">
+
+                <h2 id="project-overview">Project Overview</h2>
+		<p> </p>
+
+	<h2 id="map-generation">Pick and Place</h2>
+	<p></p>
+
+	</article>
+	</div>
+	</section>
+	</React.Fragment>
+
+  );
+}
